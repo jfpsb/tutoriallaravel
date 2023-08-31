0c26a7614522eef8f8059d31c5b5353bcf7c9eb6
Jose Felipe Silva <jose.silva.59@ebserh.gov.br>
2023-08-31 17:14:01 -0300

    adicionando rotas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resources("estudantes", EstudanteController::class);
+Route::resources("disciplinas", DisciplinaController::class);
+Route::resources("provas", ProvaController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
